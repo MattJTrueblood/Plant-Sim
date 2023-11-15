29793dd9ce89423f3c8f6139a6a63b4d7e0b21ab
MattJTrueblood <mattjtrueblood@gmail.com>
2023-11-16 03:58:11 +0700

    adding some simple boilerplate code for the project

---
+++ encoder.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def save_frames(frames, file_name):
+    """
+    Saves a sequence of frames (images) as an animation (GIF or video).
+
+    :param frames: A list or sequence of frames (images).
+    :param file_name: The name of the file to save the animation.
+    """
+    # TODO
+    pass
---
+++ plant_sim.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def create_world(width, height):
+    """
+    Creates the initial grid for the simulation.
+
+    :param width: Width of the world.
+    :param height: Height of the world.
+    :return: The created world (grid).
+    """
+    # TODO
+    pass
+
+def run_sim(world, num_steps):
+    """
+    Runs the simulation for a given number of steps.
+
+    :param world: The world (grid) on which to run the simulation.
+    :param num_steps: Number of steps to run the simulation.
+    :return: A numpy array of frames for each step of the simulation
+    """
+    # TODO
+    pass
